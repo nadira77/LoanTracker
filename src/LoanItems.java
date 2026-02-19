309440efe551c5891775c5f58fe2f08199aa1dfc
CilleElisabeth <cime1000@stud.ek.dk>
2026-02-19 11:19:08 +0100

    LoanItems påbegyndt

--- src/LoanItems.java
+++ src/LoanItems.java
@@ -1,2 +1,71 @@
-public class LoanItems {
+public abstract class LoanItems {
+    private String type;
+    private String title;
+    private double price;
+    private int year;
+    private int loandays;
+
+    //Konstruktør
+
+    public LoanItems(String type, String title, double price, int year, int loandays) {
+        this.type = type;
+        this.title = title;
+        this.price = price;
+        this.year = year;
+        this.loandays = loandays;
+
+    }
+
+    //Getters and setters
+
+    public String getType(){
+        return type;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getLoandays(){
+        return loandays;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setLoandays(int loandays) {
+        this.loandays = loandays;
+    }
+
+    //Abstrakte metoder
+    public abstract String getDecription();
+
+    public abstract double feeAmount();
+
+    //toString() metode
+    @Override
+
+
+
 }
