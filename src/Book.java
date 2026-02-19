24f4109a6c6480e9bacda41b028a62807a5bd387
LukasBager <lukasemilbager@protonmail.com>
2026-02-19 11:47:37 +0100

    Book.java first draft

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,20 @@
-public class Book {
+public class Book extends LoanItems {
+
+    private final String author;
+
+    public Book(String type, String title, double price, int year, int loandays, String author) {
+        super(type, title, price, year, loandays);
+        this.author = author;
+    }
+
+    @Override
+    public String getDecription() {
+        return this.getType() + ": " + this.getTitle() + "(" + this.getYear() + ") by: " + author + " at price: $" + this.getPrice() + ". Days loaned: " + this.getLoandays();
+    }
+
+    @Override
+    public double feeAmount() {
+        return this.getPrice() * 0.3;
+    }
+
 }
