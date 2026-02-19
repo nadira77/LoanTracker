6e6598cbfbc4df883342c3a865d48ef66d3f996a
nadir77 <nahi1000@stud.ek.dk>
2026-02-19 11:24:44 +0100

    Loansystem

--- src/LoanSystem.java
+++ src/LoanSystem.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class LoanSystem {
+    //*
+// * Test class demonstrating:
+// * - Abstraction
+// * - Inheritance
+// * - Polymorphism
+// * - Arrays
+// * - Manual sorting
+// * - Input validation
+// */
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hvor mange items vil du låne?");
+
+        int amount = sc.nextInt();
+
+
+        LoanSystem[] LoanSystemarray;
+        SortBy
+
+    }
 }
+
