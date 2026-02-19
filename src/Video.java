54b08ceff30f10859e5baaa15f328f8535325a34
CilleElisabeth <cime1000@stud.ek.dk>
2026-02-19 12:49:52 +0100

    Video færdig

--- src/Video.java
+++ src/Video.java
@@ -1,6 +1,20 @@
 public class Video extends LoanItems {
 
-    private double playtime;
+    private final double runtime;
 
-    public
+    public Video(String type, String title, double price, int year, int loandays, double runtime) {
+        super(type, title, price, year, loandays);
+        this.runtime = runtime;
+    }
+
+    @Override
+    public String getDecription() {
+        return this.getType() + ": " + this.getTitle() + "(" + this.getYear() + ") runtime: " + runtime + " at price: $" + this.getPrice() + ". Days loaned: " + this.getLoandays();
+    }
+
+    @Override
+    public double feeAmount() {
+        return this.getPrice() * 0.5;
+    }
+
 }
