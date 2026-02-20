b54f9a1af0a3e7b5b75a17e8bc131170c5b8badc
nadir77 <nahi1000@stud.ek.dk>
2026-02-20 09:29:53 +0100

    Loansystem

--- src/Book.java
+++ src/Book.java
@@ -1,27 +1,2 @@
-public class Book extends LoanItems {
-
-    private final String author;
-
-    public Book(String type, String title, double price, int year, int loandays, String author) {
-        super(type, title, price, year, loandays);
-        this.author = author;
-    }
-
-    @Override
-    public String getDescription() {
-        return this.getType() + ": " + this.getTitle() + "(" + this.getYear() + ") by: " + author + " at price: $" + this.getPrice() + ". Days loaned: " + this.getLoandays();
-    }
-
-    // Metoden fra LoanItems bliver genbrugt her
-    @Override
-    public double feeAmount() {
-        return calculateFee(0.3);
-    }
-
-
-    @Override
-    public String toString() {
-        return super.toString() + " | Author: " + author;
-    }
-
+public class Book {
 }
--- src/LoanSystem.java
+++ src/LoanSystem.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static java.lang.IO.println;
+
 public class LoanSystem {
     //*
 // * Test class demonstrating:
@@ -17,11 +19,80 @@ public class LoanSystem {
         System.out.println("Hvor mange items vil du låne?");
 
         int amount = sc.nextInt();
+        //validere brugerens input
+        if (amount < 0 || !sc.hasNextInt()) {
+            System.out.println("Du skal skrive et positivt tal");
+        }
 
+        LoanItems[] arr = new LoanItems[amount];
 
-        LoanSystem[] LoanSystemarray;
-        SortBy
+        for (int i = 0; i < amount; i++) {
 
+            System.out.println("\nType (Book/Video/Elektronickit) ");
+            String type = sc.nextLine();
+            System.out.print("Type: ");
+            String Type = sc.nextLine();
+
+            System.out.println("Indtast Title");
+            String title = sc.nextLine();
+
+            System.out.print("Price: ");
+            double price = sc.nextDouble();
+            sc.nextLine();
+            System.out.print("Year: ");
+            String year = sc.nextLine();
+
+            System.out.print("Loandays");
+            String Loandays = sc.nextLine();
+            //opret objekt
+            if (type.equalsIgnoreCase("Book")) {
+                items[i] = new Book(title, price, year, loanDays);
+
+            } else if (type.equalsIgnoreCase("Video")) {
+                items[i] = new Video(title, price, year, loanDays);
+
+            } else {
+                items[i] = new Elektronickit(title, price, year, loanDays);
+            }
+        }
+        public abstract class LoanItems {
+        }
+        LoanItems[] items = new LoanItems[3];
+        items[0] = new Book("Java Basics", 200, "2021", 14);
+        items[1] = new Video("OOP tutorial", 150, "2022", 7);
+        items[2] = new ElectronicKit("Arduino Kit", 500, "2023", 30);
+
+        System.out.println("---- Before sorting ----");
+        println(items);
+        sortByPrice(items);
+
+        System.out.println("\n---- After sorting ----");
+        print(items);
     }
-}
 
+        // Udskriv alle items
+    
+        System.out.println("\n--- Lånte items ---");
+        for (LoanItems item : items) {
+            System.out.println(item);
+        }
+        sc.close();
+
+    public static void print(LoanItems[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+
+    public static void sortByPrice (LoanSystem[]arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j].getPrice() > arr[j + 1].getPrice()) {
+
+                    LoanSystem temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+                }
